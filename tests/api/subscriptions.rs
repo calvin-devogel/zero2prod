3374abff71531ef5a44392b74d59bc232349871d
calvin <cdevogel25@gmail.com>
2026-01-19 19:14:22 -0600

    sending dummy emails

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -1,4 +1,8 @@
 use crate::helpers::spawn_app;
+use wiremock::{
+    matchers::{method, path},
+    Mock, ResponseTemplate,
+};
 
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
@@ -58,4 +62,24 @@ async fn subscribe_returns_400_when_fields_are_present_but_invalid() {
             description
         );
     }
+}
+
+#[tokio::test]
+async fn subscribe_sends_a_confirmation_email_for_valid_data() {
+    // arrange
+    let app = spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    // act
+    app.post_subscriptions(body.into()).await;
+
+    // assert
+    // mock asserts on drop
 }
