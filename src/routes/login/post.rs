c369d91107ed45c338b03197f73184aa0c19c25a
Calvin D. <cdevogel25@gmail.com>
2026-01-26 21:09:30 -0600

    getting there with auth and html pages

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,5 +1,8 @@
-use actix_web::{HttpResponse, http::header::LOCATION, web};
+use crate::authentication::{validate_credentials, Credentials, AuthError};
+use crate::routes::error_chain_fmt;
+use actix_web::{HttpResponse, http::{StatusCode, header::LOCATION}, ResponseError, web};
 use secrecy::SecretString;
+use sqlx::PgPool;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -7,8 +10,49 @@ pub struct FormData {
     password: SecretString,
 }
 
-pub async fn login(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::SeeOther()
+#[derive(thiserror::Error)]
+pub enum LoginError {
+    #[error("Authentication failed")]
+    AuthError(#[source] anyhow::Error),
+    #[error("Something went wrong")]
+    UnexpectedError(#[from] anyhow::Error),
+}
+
+impl std::fmt::Debug for LoginError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+impl ResponseError for LoginError {
+    fn status_code(&self) -> StatusCode {
+        match self {
+            LoginError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            LoginError::AuthError(_) => StatusCode::UNAUTHORIZED,
+        }
+    }
+}
+
+#[tracing::instrument(
+    skip(form, pool),
+    fields(username=tracing::field::Empty, user_id=tracing::field::Empty)
+)]
+pub async fn login(
+    form: web::Form<FormData>,
+    pool: web::Data<PgPool>,
+) -> Result<HttpResponse, LoginError> {
+    let credentials = Credentials {
+        username: form.0.username,
+        password: form.0.password,
+    };
+    let user_id = validate_credentials(credentials, &pool)
+        .await
+        .map_err(|e| match e {
+            AuthError::InvalidCredentials(_) => LoginError::AuthError(e.into()),
+            AuthError::UnexpectedError(_) => LoginError::UnexpectedError(e.into()),
+        })?;
+    tracing::Span::current().record("user_id", tracing::field::display(&user_id));
+    Ok(HttpResponse::SeeOther()
         .insert_header((LOCATION, "/"))
-        .finish()
+        .finish())
 }
